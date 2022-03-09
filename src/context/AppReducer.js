b980d9abdf85a7e0e5eb37bb7fbf9666d0f05878
sayed drissi <drissi.sayed8@gmail.com>
2022-03-09 03:51:16 +0100

    second commit

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,16 +1,16 @@
-export default (state,action)=>{
-    switch (action.type){
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function AppReducer(state, action) {
+    switch (action.type) {
         case "ADD_MOVIE_TO_FAVORITE":
-            return{
+            return {
                 ...state,
-                favoris:[action.payload,...state.favoris],
-
-            }
+                favoris: [action.payload, ...state.favoris],
+            };
         case "REMOVE_MOVIE_FROM_FAVORITE":
-            return{
+            return {
                 ...state,
-                favoris:state.favoris.filter(movie=>movie.id !== action.payload)
-            }
+                favoris: state.favoris.filter(movie => movie.id !== action.payload)
+            };
         default:
             return state;
     }
